3384dc3c6a2ba491955907ffa7c833ceaebc21a7
Ram Uday Kumar <56128152+ThankKumar@users.noreply.github.com>
2022-07-22 07:28:12 +0530

    class code 21-jul 2022

--- Notes/Introduction of java 21-jul-2022/code.java
+++ Notes/Introduction of java 21-jul-2022/code.java
@@ -1,1 +1,113 @@
 
+// 1. IDE - Integrated Development Environment
+// 2. Course Curriculum + Notes and Codes
+// 3. Java Programming Language - Template (High Level Language vs Low Level Language)
+// 4. Brief Idea of Java Architecture - Compilation vs Execution, JDK -> JVM -> JavaC -> JRE 
+
+// 5. Output in Java - System.out.print() vs System.out.println()
+// Printing String Literals
+// Task -> 5.1. Print Hello World in one line and character by character
+// Task -> 5.2. Print Z Pattern
+// Task -> 5.3. Homework: Print H pattern
+
+// 6. Arithmetic Operations - +, -, /, *, %
+// Task -> 6.1. Integer Divison vs Float Division
+// Task -> 6.2. Last Digit of Number
+// Task -> 6.3. Homework: Second Last Digit of Number
+
+// 7.1. Variables and Datatypes (Primitives)
+// 7.2. Assignment Operation (=)
+// 7.3. Arithmetic Operations on Integers and Floats
+// Task -> Add 2 String Literals, Add 2 Integers, String Literal with Integer, Integer with Float
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args){
+        // We will write the entire here 
+        
+//         // Output : ln = new line
+//         System.out.println("Archit Aggarwal says Hello");
+//         System.out.println("I am Geekster Educator.");
+        
+//         // Output will be in the same line
+//         System.out.print("Hey, I will teach you DSA. ");
+//         System.out.print("I will also teach you OOPS.");
+        
+        // System.out.print("Sachin");
+        // System.out.println("Ramesh");
+        // System.out.print("Tendulkar");
+        
+        // Printing Hello World Line by Line
+        // System.out.println("H");
+        // System.out.println("E");
+        // System.out.println("L");
+        // System.out.println("L");
+        // System.out.println("O");
+        // System.out.println();
+        // System.out.println("W");
+        // System.out.println("O");
+        // System.out.println("R");
+        // System.out.println("L");
+        // System.out.println("D");
+        
+        // Arithmetic Operations : +, -, /, *, %
+        // System.out.println(10 + 20); // Integer + Integer = Integer
+        // System.out.println(10.5 + 3.7); // Float + Float = Float
+        // System.out.println(10 + 20.5); // Integer + Float = Float
+        // System.out.println(10.5 + 30.5);
+        
+        // System.out.println(30 - 10);
+        // System.out.println(10 - 30);
+        // System.out.println(30 - 10.5);
+        // System.out.println(30.5 - 10);
+        // System.out.println(30.5 - 10.5);
+        
+        // System.out.println(30 * 10);
+        // System.out.println(10 * 30);
+        // System.out.println(30 * 10.5);
+        // System.out.println(30.5 * 10);
+        // System.out.println(30.5 * 10.5);
+        
+        // Integer Division
+//         System.out.println(30 / 10);
+//         System.out.println(30 / 20); 
+//         // Loss of Data (1.5 => .5 will be discarded)
+//         System.out.println(10 / 20); 
+//         // Loss of Data (0.5 => .5 will be discarded)
+//         System.out.println(10 / 30); 
+//         // Loss of Data (0.333 => .333 will be discarded)
+        
+//         // Float Divison (No Loss of Data)
+//         System.out.println(30.0 / 10);
+//         System.out.println(30 / 20.0);
+//         System.out.println(10.0 / 20);
+//         System.out.println(10.0 / 30.0);
+        
+        // Modulus Operation (%)
+        // Division -> Remainder
+        
+        // Integer Modulo
+//         System.out.println(30 % 10); // 30 = 10 * 3 + 0
+//         System.out.println(30 % 20); // 30 = 20 * 1 + 10
+//         System.out.println(10 % 20); // 10 = 20 * 0 + 10
+//         System.out.println(10 % 30); // 10 = 30 * 0 + 10
+//         System.out.println();
+        
+//         System.out.println(30 % 2); // 30 = 15 * 2 + 0
+//         System.out.println(35 % 2); // 35 = 17 * 2 + 1
+        
+        // Odd Numbers % 2 => 1
+        // Even Number % 2 => 0
+        
+        // System.out.println("*****");
+        // System.out.println("   *");
+        // System.out.println("  *");
+        // System.out.println(" *");
+        // System.out.println("*****");
+        
+        
+    }
+}
